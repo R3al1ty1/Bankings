728a6974d14161d4418337b785b19e76af067ea3
pepes <42584383+R3al1ty1@users.noreply.github.com>
2024-01-19 03:36:09 +0300

    Update views.py

--- bmstu/views.py
+++ bmstu/views.py
@@ -2,41 +2,41 @@ from django.db.models import Q
 from django.http import HttpResponse, HttpResponseRedirect
 from datetime import date
 from django.shortcuts import redirect, render
-from bmstu_lab.models import Account, AccountStatus, ApplicationStatus, Applications, Users, SaveTerms, CardTerms, CreditTerms, DepositTerms
+from bmstu_lab.models import Agreement, AgreementStatus, ApplicationStatus, Applications, Users
 
-def getAccountIcon(request, account_id):
-    account = Account.objects.get(id=account_id)
-    if account.icon:
-        response = HttpResponse(account.icon, content_type='image/png')
+def getAgreementIcon(request, Agreement_id):
+    Agreement = Agreement.objects.get(id=Agreement_id)
+    if Agreement.icon:
+        response = HttpResponse(Agreement.icon, content_type='image/png')
     else:
         response = HttpResponse(status=204)
 
     return response
 
-def freezeAccount(request, account_name):
+def freezeAgreement(request, Agreement_name):
     try:
-        account = Account.objects.get(name=account_name)
-    except Account.DoesNotExist:
+        Agreement = Agreement.objects.get(name=Agreement_name)
+    except Agreement.DoesNotExist:
         return render(request, 'error.html')
 
     try:
-        account.change_availability()
+        Agreement.change_availability()
     except Exception as e:
         return render(request, 'error.html')
 
-    return HttpResponseRedirect('/accounts')
+    return HttpResponseRedirect('/Agreements')
 
-def GetAccounts(request):
-    account_query = request.GET.get('account_url', '')
-    if account_query == "":
-        return render(request, 'accounts.html', {'accounts': Account.objects.all()})
+def GetAgreements(request):
+    Agreement_query = request.GET.get('Agreement_url', '')
+    if Agreement_query == "":
+        return render(request, 'Accounts.html', {'Agreements': Agreement.objects.all()})
     else:
-        found = Account.objects.filter(
-            Q(name__icontains=account_query) | Q(type__icontains=account_query)
+        found = Agreement.objects.filter(
+            Q(name__icontains=Agreement_query) | Q(type__icontains=Agreement_query)
         )
-        return render(request, 'accounts.html', {'accounts': found, 'account_url': account_query})
+        return render(request, 'Accounts.html', {'Agreements': found, 'Agreement_url': Agreement_query})
 
 
-def GetAccount(request, name):
-    account = Account.objects.get(name=name)
-    return render(request, 'card.html', {'account': account, 'card_terms': card_terms})
+def GetAgreement(request, name):
+    Agreement = Agreement.objects.get(name=name)
+    return render(request, 'card.html', {'Agreement': Agreement, 'card_terms': card_terms})
