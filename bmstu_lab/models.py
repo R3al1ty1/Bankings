d8eeb222d870cbf336fa8a44e15830675b66c012
pepes <42584383+R3al1ty1@users.noreply.github.com>
2024-01-19 03:35:06 +0300

    Update models.py

--- bmstu_lab/models.py
+++ bmstu_lab/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Account(models.Model):
+class Agreement(models.Model):
     id = models.BigIntegerField(primary_key=True)
     type = models.TextField()
     name = models.TextField()
@@ -9,7 +9,7 @@ class Account(models.Model):
     number = models.BigIntegerField(unique=True)
     currency = models.IntegerField()
     bic = models.IntegerField()
-    account_status_refer = models.BigIntegerField(unique=True)
+    Agreement_status_refer = models.BigIntegerField(unique=True)
     user_id_refer = models.ForeignKey('Users', models.DO_NOTHING, db_column='user_id_refer')
     icon = models.BinaryField(blank=True, null=True)
     available = models.BooleanField(blank=True, null=True)
@@ -22,7 +22,7 @@ class Account(models.Model):
         self.save()
     class Meta:
         managed = False
-        db_table = 'account'
+        db_table = 'Agreement'
 
     def get_currency_symbol(self):
         currencyDct = {810: "₽", 840: "$", 978: "€"}
@@ -35,23 +35,23 @@ class Account(models.Model):
             return str(self.amount)
 
 
-class AccountApplication(models.Model):
+class AgreementApplication(models.Model):
     id = models.BigIntegerField(primary_key=True)
     application = models.OneToOneField('Applications', models.DO_NOTHING)
-    account = models.OneToOneField(Account, models.DO_NOTHING)
+    Agreement = models.OneToOneField(Agreement, models.DO_NOTHING)
 
     class Meta:
         managed = False
-        db_table = 'account_application'
+        db_table = 'Agreement_application'
 
 
-class AccountStatus(models.Model):
-    id = models.OneToOneField(Account, models.DO_NOTHING, db_column='id', primary_key=True)
+class AgreementStatus(models.Model):
+    id = models.OneToOneField(Agreement, models.DO_NOTHING, db_column='id', primary_key=True)
     name = models.TextField()
 
     class Meta:
         managed = False
-        db_table = 'account_status'
+        db_table = 'Agreement_status'
 
 
 class ApplicationStatus(models.Model):
@@ -74,68 +74,6 @@ class Applications(models.Model):
     class Meta:
         managed = False
         db_table = 'applications'
-
-
-class CardTerms(models.Model):
-    id = models.BigIntegerField(primary_key=True)
-    number = models.BigIntegerField()
-    cvv = models.IntegerField()
-    holder_first_name = models.TextField()
-    holder_last_name = models.TextField()
-    maintenance_cost = models.IntegerField(blank=True, null=True)
-    exp_date = models.DateField()
-    number_ref = models.ForeignKey(Account, models.DO_NOTHING, db_column='number_ref', to_field='number', blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'card_terms'
-
-
-class CreditTerms(models.Model):
-    id = models.BigIntegerField(primary_key=True)
-    interest_rate = models.FloatField()
-    payment_amount = models.FloatField()
-    creation_date = models.DateField()
-    end_date = models.DateField()
-    agreement = models.BinaryField(blank=True, null=True)
-    payments_number = models.IntegerField()
-    purpose = models.TextField()
-    number_ref = models.ForeignKey(Account, models.DO_NOTHING, db_column='number_ref', to_field='number', blank=True, null=True)
-
-
-    def get_payment_amount(self):
-        if int(self.payment_amount) == self.payment_amount:
-            return str(int(self.payment_amount))
-        else:
-            return str(self.payment_amount)
-
-    class Meta:
-        managed = False
-        db_table = 'credit_terms'
-
-
-class DepositTerms(models.Model):
-    id = models.BigIntegerField(primary_key=True)
-    interest_rate = models.FloatField()
-    creation_date = models.DateField()
-    end_date = models.DateField()
-    agreement = models.BinaryField(blank=True, null=True)
-    days = models.IntegerField()
-    number_ref = models.ForeignKey(Account, models.DO_NOTHING, db_column='number_ref', to_field='number', blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'deposit_terms'
-
-
-class SaveTerms(models.Model):
-    id = models.BigIntegerField(primary_key=True)
-    interest_rate = models.FloatField()
-    number_ref = models.ForeignKey(Account, models.DO_NOTHING, db_column='number_ref', to_field='number', blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'save_terms'
 
 
 class Users(models.Model):
